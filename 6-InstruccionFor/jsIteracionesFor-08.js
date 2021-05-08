13acc20763457ef1f823818a4e82def48a494cb4
SoleTancredi <tancredi620@gmail.com>
2021-05-08 12:44:58 -0300

    Resuelto

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,37 @@
 function mostrar()
 {
+	let numero;
+	let contadorDivisores = 0;
+
+	numero = parseInt(prompt("Ingrese un nùmero:"));
+
+	for(let i = 1 ; i <= numero ; i++)
+	{
+		if(numero % i == 0)
+		{
+			contadorDivisores++;
+		
+		}
+		
+	}
+
+	if(contadorDivisores == 2)
+	{
+		alert("Este nùmero es PRIMO!");
+	
+	}
+	else 
+	{
+		alert("Este nùmero NO es PRIMO");
+	}
+
+		
 
 
-	alert("ok");
 
-}//FIN DE LA FUNCIÓN
+		
+
+
+	
+
+}
